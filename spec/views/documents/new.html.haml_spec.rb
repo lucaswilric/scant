7817bcadf94dc543f885198f4693b443cc3c8368
Lucas Wilson-Richter <da.maestro@gmail.com>
2014-04-13 00:19:13 +1000

    Fix tests

--- spec/views/documents/new.html.haml_spec.rb
+++ spec/views/documents/new.html.haml_spec.rb
@@ -2,9 +2,7 @@ require 'spec_helper'
 
 describe "documents/new" do
   before(:each) do
-    assign(:document, stub_model(Document,
-      :file_name => "MyString"
-    ).as_new_record)
+    assign(:document, stub_model(Document).as_new_record)
   end
 
   it "renders new document form" do
@@ -12,7 +10,7 @@ describe "documents/new" do
 
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form[action=?][method=?]", documents_path, "post" do
-      assert_select "input#document_file_name[name=?]", "document[file_name]"
+      
     end
   end
 end
