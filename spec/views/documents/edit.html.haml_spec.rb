1e04b050bfce5e98579932b204d93d99acfdbfe4
Lucas Wilson-Richter <da.maestro@gmail.com>
2014-04-13 00:19:13 +1000

    Fix tests

--- spec/views/documents/edit.html.haml_spec.rb
+++ spec/views/documents/edit.html.haml_spec.rb
@@ -2,9 +2,7 @@ require 'spec_helper'
 
 describe "documents/edit" do
   before(:each) do
-    @document = assign(:document, stub_model(Document,
-      :file_name => "MyString"
-    ))
+    @document = assign(:document, stub_model(Document))
   end
 
   it "renders the edit document form" do
@@ -12,7 +10,7 @@ describe "documents/edit" do
 
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form[action=?][method=?]", document_path(@document), "post" do
-      assert_select "input#document_file_name[name=?]", "document[file_name]"
+      
     end
   end
 end
